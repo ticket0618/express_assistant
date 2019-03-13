2e2c264ad6f414363b89088fba0a94b29525ef89
yangzebo <yangzebo@ds.cn>
2019-03-13 23:53:01 +0800

    环境

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -6,7 +6,8 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        traceUser: true,
+        env: "prd-2fa2a4",
+        traceUser: true
       })
     }
 
