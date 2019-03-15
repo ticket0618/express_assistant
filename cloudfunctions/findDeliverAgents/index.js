5ac418ce8b27ecb169a2b41b8461d246184155be
yangzebo <yangzebo@ds.cn>
2019-03-15 23:48:13 +0800

    正式环境

--- cloudfunctions/findDeliverAgents/index.js
+++ cloudfunctions/findDeliverAgents/index.js
@@ -1,7 +1,10 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-cloud.init();
+cloud.init({
+  env: 'prd-2fa2a4'
+})
+
 const db = cloud.database();
 
 // 云函数入口函数
--- cloudfunctions/removeDeliverAgent/index.js
+++ cloudfunctions/removeDeliverAgent/index.js
@@ -1,7 +1,9 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-cloud.init()
+cloud.init({
+  env: 'prd-2fa2a4'
+})
 
 const db = cloud.database()
 
